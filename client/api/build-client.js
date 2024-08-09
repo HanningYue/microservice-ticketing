42d4e170bc75b4ee9db350e1e0d0fb46bb15cf76
TacoKilla420 <yuehanning@outlook.com>
2024-08-09 11:45:48 -0700

    change local host on server

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,15 +4,13 @@ const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     // We are on the server
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'www.jamieyue-microservice-ticketing.site',
       headers: req.headers,
     });
   } else {
     // We are on the browser
     return axios.create({
-      baseURL: 'jamieyue-microservice-ticketing.site',
-      headers: req.headers,
+      baseURL: '/',
     });
   }
 };
