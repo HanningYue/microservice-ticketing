0fd811255aaff7f25153593f9a01fef761fcd7c4
TacoKilla420 <yuehanning@outlook.com>
2024-08-09 12:05:49 -0700

    added initial render

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,15 +4,13 @@ const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     // We are on the server
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://www.jamieyue-microservice-ticketing.site',
       headers: req.headers,
     });
   } else {
     // We are on the browser
     return axios.create({
       baseURL: '/',
-      withCredentials: true, // Ensure credentials are sent
     });
   }
 };
